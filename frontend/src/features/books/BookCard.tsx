095acb6927ef798e6137f875480e6ede374366d7
Chi Nguyen <chi.nguyen@live.com>
2023-08-22 13:53:26 +0300

    features/books - add bookcard component

--- frontend/src/features/books/BookCard.tsx
+++ frontend/src/features/books/BookCard.tsx
@@ -4,8 +4,50 @@ interface Props {
   book: Book;
 }
 
+// Component to display each book info
 const BookCard = (props: Props) => {
-  return <div>{props.book.title}</div>;
+  return (
+    <div className="col-md-4 col-12 p-4">
+      <div className="card" style={{ boxShadow: '0 1px 7px 0 rgb(0 0 0 /50%' }}>
+        <div className="card-body pt-2">
+          <div className="row col-10 offset-1 p-4">
+            {/* book image  */}
+            <img
+              src={props.book.imageUrl}
+              style={{ borderRadius: '5%' }}
+              alt={props.book.title}
+              className="w-100 mt-5 image-box"
+            />
+          </div>
+
+          <i
+            className="bi bi-cart-plus btn btn-outline-danger"
+            style={{
+              position: 'absolute',
+              top: '15px',
+              right: '15px',
+              padding: '5px 10px',
+              borderRadius: '3px',
+              outline: 'none !important',
+              cursor: 'pointer',
+            }}
+          ></i>
+
+          <div className="text-center">
+            <p className="card-title m-0 text-success fs-3">
+              {props.book.title}
+            </p>
+            <p className="badge bg-secondary" style={{ fontSize: '12px' }}>
+              {props.book.category.name}
+            </p>
+            <div className="row text-center">
+              <h4>${props.book.unitPrice}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookCard;
